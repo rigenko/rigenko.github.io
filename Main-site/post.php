105dc2be73005c19ace0bbc3eee3978ba2d4ab48
rigenko <62301943+rigenko@users.noreply.github.com>
2020-03-22 17:18:57 +0200

    Update post.php

--- Main-site/post.php
+++ Main-site/post.php
@@ -1,10 +1,7 @@
 <?php 
-
 if (!empty($_POST['name']) AND !empty($_POST['email']) AND !empty($_POST['phone']) AND !empty($_POST['kod'])){
 
-	$headers =	'From: Вестка сайтов\r\n'.
-	          	'Reply-to: saka150577@gmail.com\r\n'.
-	          	'X-Mailer: PHP/'. phpversion();
+	
 
 	$theme = 'Новое сообщение';
 
@@ -12,7 +9,7 @@ if (!empty($_POST['name']) AND !empty($_POST['email']) AND !empty($_POST['phone'
 	$letter .= 'Имя: '.$_POST['name'].'\r\n';
 	$letter .= 'Email: '.$_POST['email'].'\r\n';
 	$letter .= 'Телефон: '.$_POST['phone'].'\r\n';
-	$letter .= 'Код товаа: '.$_POST['kod'].'\r\n';
+	$letter .= 'Код товара: '.$_POST['kod'].'\r\n';
 
 	if (mail('saka150577@gmail.com', $theme, $letter, $headers)){
 		header('Location:/sent.php');
