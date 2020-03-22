34a906c6b19a53b6a4fc5b661a9dbecf37a35785
rigenko <62301943+rigenko@users.noreply.github.com>
2020-03-22 16:49:47 +0200

    Update mail.php

--- Main-site/mail.php
+++ Main-site/mail.php
@@ -15,11 +15,7 @@ if (!empty($_POST['name']) AND !empty($_POST['email']) AND !empty($_POST['phone'
 	$letter .= 'Код товаа: '.$_POST['kod'].'\r\n';
 
 if (mail('saka150577@gmail.com', $theme, $letter, $headers)){
-	header('Location:/sent.php');
-} else {
-	header('Location:/eror.php');
-	
-}
+} 
 }
 ?>
 
