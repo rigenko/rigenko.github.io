f1d09a602e4c53febcfd95a1f300341402c06a40
rigenko <62301943+rigenko@users.noreply.github.com>
2020-03-22 17:54:05 +0200

    Update post.php

--- Main-site/post.php
+++ Main-site/post.php
@@ -1,16 +1,14 @@
 <?php
 $name = $_POST['name'];
 $email = $_POST['email'];
-$fio = htmlspecialchars($fio);
+$name = htmlspecialchars($name);
 $email = htmlspecialchars($email);
-$fio = urldecode($fio);
+$name = urldecode($name);
 $email = urldecode($email);
-$fio = trim($fio);
+$name = trim($name);
 $email = trim($email);
-//echo $fio;
-//echo "<br>";
-//echo $email;
-if (mail("saka150577@gmail.com", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: saka150577@gmail.com \r\n"))
+
+if (mail("saka150577@gmail.com", "Заявка с сайта", "ФИО:".$name.". E-mail: ".$email ,"From: saka150577@gmail.com \r\n"))
  {     echo "сообщение успешно отправлено";
 } else {
     echo "при отправке сообщения возникли ошибки";
